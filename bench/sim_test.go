eb531d461b46fbfce8944c6d47105c2bd9dde422
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2025-08-28 00:01:27 -0300

    Update bench module to use runner.Sim accordingly

--- bench/sim_test.go
+++ bench/sim_test.go
@@ -9,11 +9,12 @@ import (
 const tracePath = "../azure/sample-100k-processed.csv"
 const outputPath = "../results/"
 
-var forwardLatency = []int{1}
+var forwardLatency = []int{0}
+var skipColdStart = []bool{true}
 
 func BenchmarkSimulator(b *testing.B) {
 	technique := []string{"GCI"}
 	percentileThreshould := []string{"p99"}
 	replicaIdleTime := []int{-1}
-	runner.Sim(tracePath, outputPath, technique, percentileThreshould, replicaIdleTime, forwardLatency)
+	runner.Sim(tracePath, outputPath, technique, percentileThreshould, replicaIdleTime, forwardLatency, skipColdStart)
 }

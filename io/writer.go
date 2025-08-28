79e63bd4c71f2d289d57afc5e300f7afc221bc68
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2025-08-28 00:02:21 -0300

    Enable output writing by rows on io module

--- io/writer.go
+++ io/writer.go
@@ -28,3 +28,31 @@ func WriteOutput(outputPath, simulationName string, data [][]string) {
 		}
 	}
 }
+
+func WriteOutputHeaderRow(outputPath, simulationName string, rows []string) {
+	_ = os.Remove(outputPath + simulationName)
+	WriteOutputByRow(outputPath, simulationName, rows)
+}
+
+func WriteOutputByRow(outputPath, simulationName string, rows []string) {
+	err := os.MkdirAll(outputPath, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	filePath := outputPath + simulationName
+	// Open the file in append mode, create if not exists
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	err = writer.Write(rows)
+	if err != nil {
+		panic(err)
+	}
+}
